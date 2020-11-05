d22da5dfee27bb2a3476caa876dc482057591263
George Cave <gcave@stablecoder.ca>
2020-11-04 20:10:38 -0500

    Added headers for v1.2.159

--- include/vk_error_code.hpp
+++ include/vk_error_code.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 159
+    #include "detail_error_code/vk_error_code_v159.hpp"
+#endif
 #if VK_HEADER_VERSION == 158
     #include "detail_error_code/vk_error_code_v158.hpp"
 #endif
