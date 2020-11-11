6fdc22f70778ba490b2bcd2232c296644582b21d
George Cave <gcave@stablecoder.ca>
2020-11-11 14:24:51 -0500

    Added v1.2.160 headers.

--- include/vk_equality_checks.hpp
+++ include/vk_equality_checks.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 160
+    #include "detail_equality_checks/vk_equality_checks_v160.hpp"
+#endif
 #if VK_HEADER_VERSION == 159
     #include "detail_equality_checks/vk_equality_checks_v159.hpp"
 #endif
--- include/vk_value_serialization.hpp
+++ include/vk_value_serialization.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 160
+    #include "detail_value_serialization/vk_value_serialization_v160.hpp"
+#endif
 #if VK_HEADER_VERSION == 159
     #include "detail_value_serialization/vk_value_serialization_v159.hpp"
 #endif
