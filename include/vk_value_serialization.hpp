79a9ceef25bc56621eed924bc9504fc96aea0fbb
George Cave <gcave@stablecoder.ca>
2021-03-02 18:46:59 -0500

    Add v1.2.171 headers.

--- include/vk_value_serialization.hpp
+++ include/vk_value_serialization.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 171
+    #include "detail_value_serialization/vk_value_serialization_v171.hpp"
+#endif
 #if VK_HEADER_VERSION == 170
     #include "detail_value_serialization/vk_value_serialization_v170.hpp"
 #endif
