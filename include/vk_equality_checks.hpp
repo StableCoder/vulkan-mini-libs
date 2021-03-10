e8fbc85a7e404fa1bdc6d4f3680ac5a8a88e8b98
George Cave <gcave@stablecoder.ca>
2021-03-09 22:43:19 -0500

    Added v1.2.172 headers.

--- include/vk_equality_checks.hpp
+++ include/vk_equality_checks.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 172
+    #include "detail_equality_checks/vk_equality_checks_v172.hpp"
+#endif
 #if VK_HEADER_VERSION == 171
     #include "detail_equality_checks/vk_equality_checks_v171.hpp"
 #endif
--- include/vk_value_serialization.hpp
+++ include/vk_value_serialization.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 172
+    #include "detail_value_serialization/vk_value_serialization_v172.hpp"
+#endif
 #if VK_HEADER_VERSION == 171
     #include "detail_value_serialization/vk_value_serialization_v171.hpp"
 #endif
