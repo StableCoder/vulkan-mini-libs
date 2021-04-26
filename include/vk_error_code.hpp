42b6e18858ce2d3aa6d54fd363647cc980e2dc34
George Cave <gcave@stablecoder.ca>
2021-04-26 18:23:18 -0400

    Added v1.2.177 headers.

--- include/vk_error_code.hpp
+++ include/vk_error_code.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 177
+    #include "detail_error_code/vk_error_code_v177.hpp"
+#endif
 #if VK_HEADER_VERSION == 176
     #include "detail_error_code/vk_error_code_v176.hpp"
 #endif
