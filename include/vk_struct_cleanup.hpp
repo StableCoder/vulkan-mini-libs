2028a9ae4b54f37e3f02072ea5c87801ce1d209d
George Cave <gcave@stablecoder.ca>
2021-06-12 20:45:57 -0400

    Added v1.2.180 headers

--- include/vk_struct_cleanup.hpp
+++ include/vk_struct_cleanup.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 180
+    #include "detail_struct_cleanup/vk_struct_cleanup_v180.hpp"
+#endif
 #if VK_HEADER_VERSION == 179
     #include "detail_struct_cleanup/vk_struct_cleanup_v179.hpp"
 #endif
