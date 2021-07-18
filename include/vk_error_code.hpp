d67f345e67745b6cd9b278f9874302775013fa73
George Cave <gcave@stablecoder.ca>
2021-07-18 15:22:04 -0400

    Added v1.2.183 headers

--- include/vk_error_code.hpp
+++ include/vk_error_code.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 183
+    #include "detail_error_code/vk_error_code_v183.hpp"
+#endif
 #if VK_HEADER_VERSION == 182
     #include "detail_error_code/vk_error_code_v182.hpp"
 #endif
