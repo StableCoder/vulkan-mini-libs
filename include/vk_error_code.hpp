f01149f2030b7b160b4a2042396567d7b90fa992
George Cave <gcave@stablecoder.ca>
2021-08-30 00:34:14 -0400

    Added v1.2.190 headers.

--- include/vk_error_code.hpp
+++ include/vk_error_code.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 190
+    #include "detail_error_code/vk_error_code_v190.hpp"
+#endif
 #if VK_HEADER_VERSION == 189
     #include "detail_error_code/vk_error_code_v189.hpp"
 #endif
