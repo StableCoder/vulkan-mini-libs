4a9fe051b9599ecc09a35793e6275b2bd45433af
George Cave <gcave@stablecoder.ca>
2021-09-26 17:33:47 -0400

    Added v1.2.193 headers

--- include/vk_struct_cleanup.hpp
+++ include/vk_struct_cleanup.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 193
+    #include "detail_struct_cleanup/vk_struct_cleanup_v193.hpp"
+#endif
 #if VK_HEADER_VERSION == 192
     #include "detail_struct_cleanup/vk_struct_cleanup_v192.hpp"
 #endif
--- include/vk_value_serialization.hpp
+++ include/vk_value_serialization.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 193
+    #include "detail_value_serialization/vk_value_serialization_v193.hpp"
+#endif
 #if VK_HEADER_VERSION == 192
     #include "detail_value_serialization/vk_value_serialization_v192.hpp"
 #endif
