4582c506ce4603dff899c7498c649923e24baa27
George Cave <gcave@stablecoder.ca>
2021-11-14 17:48:13 -0500

    Added v1.2.197 headers

--- include/vk_equality_checks.hpp
+++ include/vk_equality_checks.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 197
+    #include "detail_equality_checks/vk_equality_checks_v197.hpp"
+#endif
 #if VK_HEADER_VERSION == 196
     #include "detail_equality_checks/vk_equality_checks_v196.hpp"
 #endif
