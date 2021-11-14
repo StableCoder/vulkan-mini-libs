db84b223d43db78c3813413467fd6728f8794891
George Cave <gcave@stablecoder.ca>
2021-11-14 17:55:58 -0500

    Added v1.2.198 headers

--- include/vk_value_serialization.hpp
+++ include/vk_value_serialization.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 198
+    #include "detail_value_serialization/vk_value_serialization_v198.hpp"
+#endif
 #if VK_HEADER_VERSION == 197
     #include "detail_value_serialization/vk_value_serialization_v197.hpp"
 #endif
