e1e5e168a25abd40e3827e00b61238b4ece1d5a9
George Cave <gcave@stablecoder.ca>
2021-11-24 09:36:03 -0500

    Add v1.2.200 headers

--- include/vk_equality_checks.hpp
+++ include/vk_equality_checks.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 200
+    #include "detail_equality_checks/vk_equality_checks_v200.hpp"
+#endif
 #if VK_HEADER_VERSION == 199
     #include "detail_equality_checks/vk_equality_checks_v199.hpp"
 #endif
