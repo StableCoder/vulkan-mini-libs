c53888b7ca1bd9e16ad4c83614ec67669da08a8a
George Cave <gcave@stablecoder.ca>
2021-12-09 18:40:31 -0500

    Add v1.2.202 support

--- include/vk_error_code.hpp
+++ include/vk_error_code.hpp
@@ -36,6 +36,9 @@
 #include <vulkan/vulkan.h>
 
 // Delegate to header specific to the local Vulkan header version
+#if VK_HEADER_VERSION == 202
+    #include "detail_error_code/vk_error_code_v202.hpp"
+#endif
 #if VK_HEADER_VERSION == 201
     #include "detail_error_code/vk_error_code_v201.hpp"
 #endif
